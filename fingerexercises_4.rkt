4c7ef9f8d8cb980dff372b01f859142a88908bd7
HarkirtN <harkirts@gmail.com>
2025-09-16 20:06:03 +1200

    updated finger exercises

--- fingerexercises_4.rkt
+++ fingerexercises_4.rkt
@@ -101,15 +101,30 @@
                                               [(symbol=? symbol (inv-name (first inv-list))) (cons (first inv-list)(show-picture symbol (rest inv-list)))]
                                               [else (show-picture symbol (rest inv-list))]))
 
-;; phone directory that combines phone number and names
+;; phone directory that matches phone numbers and names
 (define-struct directory (name phone))
 
 ;; whose-number : symbol -> num-symbol
-(define (whose-number name directory) (cond
+(define (whose-number phone directory) (cond
                                           [(empty? directory) empty]
-                                          [(symbol=? name (directory-name (first directory)))
-                                           (cons (first directory)(whose-number name (rest directory)))]
-                                          [else (error 'whose-number "unknown number")]))
+                                          [(... phone (directory-phone (first directory)))
+                                           (directory-name (first directory))]
+                                          [else (whose-number phone (rest directory))]))
 
 ;;test
-(whose-number 'joe (cons (make-directory 'joe 021113) (cons (make-directory 'harky 02111555) (cons (make-directory 'lily 090909) empty))))
+(whose-number 021113 (cons (make-directory 'joe 021113) (cons (make-directory 'harky 02111555) (cons (make-directory 'lily 090909) empty))))
+
+
+;; extract inventory that are priced above $1
+(define (extract1 an-inv) (cond
+                            [(empty? an-inv) empty]
+                            [else (cond
+                                    [(>= ir-price(first an-inv) 1.00)
+                                    (cons (first an-inv) (extract1 (rest an-inv)))]
+                                    [else (extract1 (rest an-inv))])]))
+
+;; raise-prices : an-inv -> an-inv
+;; all prices are increased by 5%
+(define (raise-prices an-inv) (cond
+                                [(empty? an-inv) empty]
+                                [else (cons (* 0.05 (ir-price (first an-inv))) (raise-prices (rest an-inv)))]))
