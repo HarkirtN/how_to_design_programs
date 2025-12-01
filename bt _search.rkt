e0fd3e8249115f24c9eb74fe52359c37384f02df
HarkirtN <harkirts@gmail.com>
2025-12-02 12:27:17 +1300

    updated with neighbours function

--- bt _search.rkt
+++ bt _search.rkt
@@ -29,19 +29,36 @@
 ;;provides a listof all possible paths
 (define (contains-n? origin graph) (cond
                              [(null? graph) false]
-                             [else (or (= origin (node-ssn graph))
+                             [else (or (= origin (first graph))
                                         (or (contains-n? origin (node-left graph)) (contains-n? origin (node-right graph))))]))
 
 (define (neighbours origin graph) (cond
-                           [(and (null? (node-left graph)) (null? (node-right graph))) false]
-                           [else (and (contains-n? origin graph)
-                                      (cons (neighbours origin (node-left graph)) (neighbours origin (node-right graph))))]))
+                           [(empty? graph) empty]
+                           [else (cond
+                                   [(= origin (first graph)) graph]
+                                   [else (neighbours origin (rest graph))])]))
 
 ;;test
-(neighbours 29 (make-node 63 'd
-                     (make-node 29 'c
-                                (make-node 15 'b
-                                           (make-node 10 'a null null)(make-node 24 'aa null null) v)null)
-                     (make-node 89 'cc
-                                (make-node 77 'bb null null) (make-node 95 'bbb
-                                                                          (make-node 99 'dd null null) null))))
+;;(neighbours 29 (make-node 63 'd
+                     ;;(make-node 29 'c
+                               ;; (make-node 15 'b
+                                           ;;(make-node 10 'a null null)(make-node 24 'aa null null))null)
+                    ;; (make-node 89 'cc
+                    ;;            (make-node 77 'bb null null) (make-node 95 'bbb
+                                                                        ;;  (make-node 99 'dd null null) null))))
+(define (in-order n a-tree) (cond
+                            [(false? a-tree) empty]
+                            [else (append (node-left a-tree) (cons (node-name a-tree) (in-order n (node-right a-tree))))]))
+                                    
+ ;;test - doesnt work yet only provides me with list of nodes regardless of the node origin 
+ (neighbours o graph1) ;;= (list D empty)
+
+;; find path : node node graph -> listof nodes or false
+ ;; computes a path going through the neighbours to destination or returns false
+ (define (find-path origin destination G) (cond
+                                            [(symbol=? origin destination) (list destination)]
+                                            [else (local ((define possible-path (find-path (neighbours origin G) destination G)))
+                                                    (cond
+                                                      [(boolean? possible-path) false]      ;;if it failed to find route using neighbours it is impossible to reach and returns false 
+                                                      [else (cons origin possible-path)]))])) ;;if found then returns the listof nodes and attaches to the origin 
+
